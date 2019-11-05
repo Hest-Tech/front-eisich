3921f2f9ecdd8cb6943668bbd72752d499f891de
Dave <macharia3041@gmail.com>
2019-11-05 10:40:03 +0300

    finishing up index page

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -13,34 +13,68 @@ const Trending = () => (
             <h4 className="display-4">Trending</h4>
             <div className="trending">
                 <div className="trending__container">
-                    <img src="arrow-left.svg" className="arrow arrow-left"/>
-                    <img src="arrow-right.svg"className="arrow arrow-right" />
+                    <img src="arrow-left.svg" className="arrow arrow-left" />
+                    <img src="arrow-right.svg" className="arrow arrow-right" />
                     <div className="trending-img-slider__container">
                         <div className="trending-img">
                             <div className="img-overlay">
                                 <img src="shoes.jpg" className="trending-img-item" />
                             </div>
+                            <div className="promo-info">
+                                <span>NEW</span>
+                            </div>
                             <div className="trending-img-content">
-                                adfl;dfal;asldfkj
+                                <b>Short sleeve Abaya</b>
+                                <p>KSH2500</p>
+                                <div className="text-warning">
+                                    <i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i>
+                                </div>
                             </div>
                         </div>
                         <div className="trending-img">
                             <div className="img-overlay">
                                 <img src="shoes.jpg" className="trending-img-item" />
                             </div>
-                            <div className="trending-img-content">sdfgsdgf</div>
+                            <div className="promo-info">
+                                <span>20% OFF</span>
+                            </div>
+                            <div className="trending-img-content">
+                                <b>Short sleeve Abaya</b>
+                                <p>KSH2500</p>
+                                <div className="text-warning">
+                                    <i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i>
+                                </div>
+                            </div>
                         </div>
                         <div className="trending-img">
                             <div className="img-overlay">
                                 <img src="shoes.jpg" className="trending-img-item" />
                             </div>
-                            <div className="trending-img-content"></div>
+                            <div className="promo-info">
+                                <span>NEW</span>
+                            </div>
+                            <div className="trending-img-content">
+                                <b>Short sleeve Abaya</b>
+                                <p>KSH2500</p>
+                                <div className="text-warning">
+                                    <i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i>
+                                </div>
+                            </div>
                         </div>
                         <div className="trending-img">
                             <div className="img-overlay">
                                 <img src="shoes.jpg" className="trending-img-item" />
                             </div>
-                            <div className="trending-img-content"></div>
+                            <div className="promo-info">
+                                <span>20% OFF</span>
+                            </div>
+                            <div className="trending-img-content">
+                                <b>Short sleeve Abaya</b>
+                                <p>KSH2500</p>
+                                <div className="text-warning">
+                                    <i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
