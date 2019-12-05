804bceffcbd8b39442c7167f60b1257d6cc4e41d
David Macharia <macharia3041@gmail.com>
2019-12-05 11:35:29 +0300

    responsive mobile nav bar

--- src/components/home/MobileNav.js
+++ src/components/home/MobileNav.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
+
 const MobileNav = () => {
     return (
-        <div className="mobile-nav-bar flex-mobile">dsagh</div>
+        <div className="mobile-nav-bar flex-mobile">
+            <div className="mobile-nav-bar__background">
+                <div className="mobile-icon-background mobile-home active-icon">
+                    <i className="mobile-icon fas fa-home"></i>
+                    <p className="icon-det">Home</p>
+                </div>
+                <div className="mobile-icon-background mobile-category">
+                    <i className="mobile-icon fas fa-bars"></i>
+                    <p className="icon-det">Category</p>
+                </div>
+                <div className="mobile-icon-background mobile-cart">
+                    <i className="mobile-icon fas fa-cart-arrow-down"></i>
+                    <p className="icon-det">Cart</p>
+                </div>
+                <div className="mobile-icon-background mobile-account">
+                    <i className="mobile-icon fas fa-user"></i>
+                    <p className="icon-det">Account</p>
+                </div>
+            </div>
+        </div>
     );
 }
 
